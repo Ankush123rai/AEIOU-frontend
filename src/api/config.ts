94d7fe95bf7d98226d6ae80f1cade595e3a3046f
ankush rai <ankush98rai@gmail.com>
2026-01-27 12:01:49 +0530

    changes done

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,4 +1,4 @@
-// export const PROXY_BASE_URL = 'https://api-fieldbuddy.premacg.com'
-export const PROXY_BASE_URL = 'http://localhost:8080'
+export const PROXY_BASE_URL = 'https://api.aeiou.org.in'
+// export const PROXY_BASE_URL = 'http://localhost:8080'
 
 export const API_BASE_URL = `${PROXY_BASE_URL}/api/`; 
